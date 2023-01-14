2ea4d4933cac75d9f6994d70934b081e38fbcdc0
serdcelosya <114828013+serdcelosya@users.noreply.github.com>
2023-01-14 15:13:35 +0000

    Deploying to gh-pages from @ serdcelosya/ice-team@680d93c838fa86aa5ae98720d1af3f0f45b875dc 🚀

---
+++ index.08e8c057.js
@@ -0,0 +1,2 @@
+$(".reviews-js").slick({dots:!0,infinite:!0,speed:300,slidesToShow:1,adaptiveHeight:!0,arrows:!1});
+//# sourceMappingURL=index.08e8c057.js.map
---
+++ index.9985d383.js
@@ -0,0 +1,2 @@
+$(".reviews-js").slick({dots:!0,infinite:!0,speed:300,slidesToShow:1,adaptiveHeight:!0,arrows:!1});
+//# sourceMappingURL=index.9985d383.js.map
