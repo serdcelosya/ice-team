8ffde76e33108871eed88d8d3c1d79e15342957c
serdcelosya <114828013+serdcelosya@users.noreply.github.com>
2023-01-16 16:55:40 +0000

    Deploying to gh-pages from @ serdcelosya/ice-team@45197327eb47a389cfdc058d9378f7e11194958e 🚀

---
+++ index.d059d077.js
@@ -0,0 +1,2 @@
+!function(){var e={openModalBtn:document.querySelector("[data-modal-open-1]"),closeModalBtn:document.querySelector("[data-modal-close-1]"),modal:document.querySelector("[data-modal-buy]")};function o(){e.modal.classList.toggle("is-hidden--buy")}e.openModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o)}();
+//# sourceMappingURL=index.d059d077.js.map
