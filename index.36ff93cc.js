05903bcd175acc095e400d2efb4e9f30e6c958b1
serdcelosya <114828013+serdcelosya@users.noreply.github.com>
2023-01-16 21:13:48 +0000

    Deploying to gh-pages from @ serdcelosya/ice-team@712ebeaf74eecd08256592101344718e3a013ff1 🚀

---
+++ index.36ff93cc.js
@@ -0,0 +1,2 @@
+$(".back-to-top").click((function(){$("body,html").animate({scrollTop:0},800)})),$(window).scroll((function(){$(window).scrollTop()>350?$(".back-to-top").addClass("active"):$(".back-to-top").removeClass("active")}));
+//# sourceMappingURL=index.36ff93cc.js.map
